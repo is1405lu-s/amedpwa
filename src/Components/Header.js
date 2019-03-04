cc12e54f4c4720ed7e45aebd70d7b0ec35f19f83
luddelex <35630379+luddelex@users.noreply.github.com>
2019-03-04 12:10:16 +0100

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,37 +7,62 @@ import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
+import AuthService from './AuthService'
+import { Redirect } from 'react-router-dom'
 
 class Header extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {username: ''}
+
+        this.Auth = new AuthService()
+        this.logOut = this.logOut.bind(this)
+    }
+
+    componentDidMount(){
+        if(this.Auth.loggedIn) {
+            this.setState({username: this.Auth.getUsername() })
+        }
+
     }
 
     render() {
         return (
-          <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">Amedic</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/home">Home</Nav.Link>
-                <Nav.Link href="/patient">Find patient</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">Amedic</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/patient">Find patient</Nav.Link>
+                        {
+                            this.Auth.loggedIn() ?
+                                <NavDropdown title={`Logged in as: ${this.state.username}`} id="basic-nav-dropdown">
+                                    <NavDropdown.Item onClick={this.logOut} href="/login">Log out</NavDropdown.Item>
+                                    {/*
                   <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
+                  */}
+                                </NavDropdown> : <Nav.Link href="/login">Log in</Nav.Link>
+                        }
+
+                    </Nav>
+                    {/*
               <Form inline>
                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                 <Button variant="outline-success">Search</Button>
               </Form>
-            </Navbar.Collapse>
-          </Navbar>
+
+            */}
+                </Navbar.Collapse>
+            </Navbar>
         )
+    }
+
+    logOut(){
+        this.Auth.logout()
     }
 }
 
