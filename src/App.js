15114b8be5dc3d977edd1bb11a16b4bde0a55a4f
luddelex <35630379+luddelex@users.noreply.github.com>
2019-03-04 12:13:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,33 @@ import React, { Component } from 'react';
 import ScreeningForm from './Components/ScreeningForm'
 import Header from './Components/Header'
 
+import AuthService from './Components/AuthService'
+import withAuth from './Components/withAuth';
+const Auth = new AuthService();
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+        <div className="App">
 
-      <Header />
+          <Header />
 
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-          crossorigin="anonymous"
-        />
+          <link
+              rel="stylesheet"
+              href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+              integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+              crossorigin="anonymous"
+          />
 
-        <ScreeningForm />
-      </div>
+          <ScreeningForm />
+        </div>
     );
+  }
+
+  handleLogout(){
+    Auth.logout()
+    this.props.history.replace('/login');
   }
 }
 
-export default App;
+export default withAuth(App);
