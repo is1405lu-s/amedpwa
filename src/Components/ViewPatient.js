7be343630347aa185c05f07e01902b21610d273d
luddelex <35630379+luddelex@users.noreply.github.com>
2019-03-04 12:09:01 +0100

    Update ViewPatient.js

--- src/Components/ViewPatient.js
+++ src/Components/ViewPatient.js
@@ -1,7 +1,13 @@
 import React from 'react'
-import {Container, Table} from 'react-bootstrap'
+import {Container, Table, Button} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 
-import VisitTD from './VisitTD'
+import HSAVisitTD from './HSAVisitTD'
+import HEVisitTD from './HEVisitTD'
+import ScreeningForm from './ScreeningForm'
+import { Redirect } from 'react-router-dom'
+
+import AuthService from './AuthService';
 
 class ViewPatient extends React.Component {
     constructor(props) {
@@ -11,12 +17,13 @@ class ViewPatient extends React.Component {
             caregivers: null,
             village: {},
             HSA_visits: null,
-            HE_visits: []
+            HE_visits: null
         }
 
         this.printOutCaregivers = this.printOutCaregivers.bind(this)
         this.printOutHSAVisits = this.printOutHSAVisits.bind(this)
-        
+        this.Auth = new AuthService()
+
     }
 
     // TODO: Make sure this method loops properly and prints out also if there is multiple caregivers:
@@ -24,14 +31,12 @@ class ViewPatient extends React.Component {
         if(this.state.caregivers != null){
             return this.state.caregivers.map(function(caregiver){
                 return (
-                <div key={caregiver.ID}>
-                <p><b>Caregiver name: </b>{caregiver.name}</p>
-                <p><b>Caregiver relation to patient: </b>{caregiver.relationToPatient}</p>
-                </div>
+                    <div key={caregiver.ID}>
+                        <p><b>Caregiver name: </b>{caregiver.name}</p>
+                        <p><b>Caregiver relation to patient: </b>{caregiver.relationToPatient}</p>
+                    </div>
                 )
             })
-        } else {
-            return <h1>Hello</h1>
         }
     }
 
@@ -41,7 +46,19 @@ class ViewPatient extends React.Component {
         if(this.state.HSA_visits != null) {
             return this.state.HSA_visits.map(function(visit) {
                 return(
-                    <VisitTD visit={visit} key={visit.patientID + visit.diagnosisID + visit.HSAID + visit.symptomID} />
+                    <HSAVisitTD visit={visit} key={visit.patientID + visit.diagnosisID + visit.HSAID + visit.symptomID} />
+                )
+            })
+        }
+    }
+
+    printOutHEVisits() {
+
+        // Loop through array, printing out each visit.
+        if(this.state.HE_visits != null) {
+            return this.state.HE_visits.map(function(visit) {
+                return(
+                    <HEVisitTD visit={visit} key={visit.patientID + visit.diagnosisID + visit.expertID} />
                 )
             })
         }
@@ -50,87 +67,127 @@ class ViewPatient extends React.Component {
     componentDidMount(){
 
         // Fetch patient object.
-        fetch(`http://localhost:3000/patient/${this.props.match.params.id}`).then(res => res.json())
-        .then(
-          (fetchedPatient) => {
-              console.log(fetchedPatient)
-              this.setState({
-              patient: fetchedPatient
-            });
-          },
-          (error) => {
-            this.setState({
-              error
-            })
-          })
+        this.Auth.fetch(`http://localhost:3000/patient/${this.props.match.params.id}`)
+            .then(
+                (fetchedPatient) => {
+                    console.log(fetchedPatient)
+                    this.setState({
+                        patient: fetchedPatient
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        error
+                    })
+                })
 
         // Fetch patient caregiver object.
-        fetch(`http://localhost:3000/caregiver/${this.props.match.params.id}`).then(res => res.json())
-        .then(
-          (fetchedCaregiver) => {
-              this.setState({
-              caregivers: fetchedCaregiver
-            });
-          },
-          (error) => {
-            this.setState({
-              error
-            })
-          })
+        this.Auth.fetch(`http://localhost:3000/caregiver/${this.props.match.params.id}`)
+            .then(
+                (fetchedCaregiver) => {
+                    this.setState({
+                        caregivers: fetchedCaregiver
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        error
+                    })
+                })
 
         // Fetch patient hsa visits array.
-        fetch(`http://localhost:3000/hsavisit/${this.props.match.params.id}`).then(res => res.json())
-        .then(
-          (fetchedHSAVisits) => {
-              this.setState({
-              HSA_visits: fetchedHSAVisits
-            });
-          },
-          (error) => {
-            this.setState({
-              error
-            })
-          })   
+        this.Auth.fetch(`http://localhost:3000/hsavisit/${this.props.match.params.id}`)
+            .then(
+                (fetchedHSAVisits) => {
+                    this.setState({
+                        HSA_visits: fetchedHSAVisits
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        error
+                    })
+                })
+
+        // Fetch patient he visits array.
+        this.Auth.fetch(`http://localhost:3000/hevisit/${this.props.match.params.id}`)
+            .then(
+                (fetchedHEVisits) => {
+                    this.setState({
+                        HE_visits: fetchedHEVisits
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        error
+                    })
+                })
 
     }
 
     render() {
         return (
             <Container>
+                {
+                    this.Auth.loggedIn() ? '' : <Redirect to='/login' />
+                }
 
-            <h2>Patient details</h2>
-            <p><b>Name: </b>{this.state.patient.name}</p>
-            <p><b>Date of birth: </b>{this.state.patient.dateOfBirth}</p>
-            <p><b>Mobile No: </b>{this.state.patient.mobileNo}</p>
-            <p><b>National ID: </b>{this.state.patient.nationalID}</p>
-            <p><b>Sex: </b>{this.state.patient.sex}</p>
-            <p><b>Village: </b>{this.state.patient.villageName}</p>
+                <h2>Patient details</h2>
+                <p><b>Name: </b>{this.state.patient.name}</p>
+                <p><b>Date of birth: </b>{this.state.patient.dateOfBirth}</p>
+                <p><b>Mobile No: </b>{this.state.patient.mobileNo}</p>
+                <p><b>National ID: </b>{this.state.patient.nationalID}</p>
+                <p><b>Sex: </b>{this.state.patient.sex}</p>
+                <p><b>Village: </b>{this.state.patient.villageName}</p>
 
-            <div>
-            {
-                // Check if patient has a caregiver, if so, print out caregiver, else don't. 
-                this.printOutCaregivers()
-            }
-            </div>
+                <div>
+                    {
+                        // Check if patient has a caregiver, if so, print out caregiver, else don't.
+                        this.printOutCaregivers()
+                    }
+                </div>
 
 
-            <h2>HSA visits</h2>
-            <Table>
+                <div class="text-center">
+                    <Link to={{ pathname:'/alaf/', state: { patient: this.state.patient } }}><Button variant="primary">Fill out symptoms sheet</Button></Link>
+                    <br /><br />
+                </div>
 
-                <thead>
+                <h2>HSA visits</h2>
+                <Table>
+
+                    <thead>
                     <tr>
                         <th>Date</th>
                         <th>Diagnosis?</th>
                         <th>Treatment?</th>
                     </tr>
-                </thead>
+                    </thead>
 
-                {
-                // Check if patient has HSA visits, if so, print out visits, else don't.
-                this.printOutHSAVisits()
-                }
+                    {
+                        // Check if patient has HSA visits, if so, print out visits, else don't.
+                        this.printOutHSAVisits()
+                    }
 
-            </Table>
+                </Table>
+
+                <h2>HE visits</h2>
+                <Table>
+
+                    <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Diagnosis?</th>
+                        <th>Treatment?</th>
+                    </tr>
+                    </thead>
+
+                    {
+                        // Check if patient has HE visits, if so, print out visits, else don't.
+                        this.printOutHEVisits()
+                    }
+
+                </Table>
 
             </Container>
         )
